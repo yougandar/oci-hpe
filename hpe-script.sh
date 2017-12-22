9fe24055fd5073c6b6d74ff70dfead051c370abe
yougandar <ynaik@sysgain.com>
2017-12-22 11:54:26 +0530

    Update hpe-script.sh

--- hpe-script.sh
+++ hpe-script.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+iqn=$1
+blockIp=$2
+iqn1=$3
+blockIp1=$4
+sudo iscsiadm -m node -o new -T $iqn -p $blockIp:3260
+sudo iscsiadm -m node -o update -T $iqn -n node.startup -v automatic
+sudo iscsiadm -m node -T $iqn -p $blockIp:3260 -l
+sudo iscsiadm -m node -o new -T $iqn1 -p $blockIp1:3260
+sudo iscsiadm -m node -o update -T $iqn1 -n node.startup -v automatic
+sudo iscsiadm -m node -T $iqn1 -p $blockIp1:3260 -l
+
+sudo -i
+sudo useradd dbadmin
 
 ##Install needed Packages
 yum -y install dialog
@@ -67,29 +80,29 @@ echo deadline > /sys/block/sdc/queue/scheduler
 
 ##Setup User
 groupadd verticadba
-usermod -g verticadba $1
-chown $1:verticadba /home/$1
-chmod 755 /home/$1
-chown $1:verticadba /data
+usermod -g verticadba dbadmin
+chown dbadmin:verticadba /home/dbadmin
+chmod 755 /home/dbadmin
+chown dbadmin:verticadba /data
 echo 'export TZ="America/New_York"' >> /etc/profile
 
 ##Install Vertica
-/opt/vertica/sbin/install_vertica --accept-eula --license CE --point-to-point --dba-user $1 --dba-user-password-disabled --hosts localhost --failure-threshold NONE
+/opt/vertica/sbin/install_vertica --accept-eula --license CE --point-to-point --dba-user dbadmin --dba-user-password-disabled --hosts localhost --failure-threshold NONE
 
 
 ##Steps for Test Drive
 yum install -y dos2unix
 mkdir /data/datafiles
-chown $1:verticadba /data/datafiles
+chown dbadmin:verticadba /data/datafiles
 mkdir /data/controlfiles
-chown $1:verticadba /data/controlfiles
+chown dbadmin:verticadba /data/controlfiles
 mkdir /tmp/java
 
-echo 1step_DownloadStart >> /home/$1/stepfile
+echo 1step_DownloadStart >> /home/dbadmin/stepfile
 wget -O /tmp/java/dos2unix-6.0.3-4.el7.x86_64.rpm https://s3.amazonaws.com/verticatestdrive/dos2unix-6.0.3-4.el7.x86_64.rpm
-wget -O /home/$1/clickstreamAB.tar.gz https://s3.amazonaws.com/verticatestdrive/clickstreamAB.tar.gz
-wget -O /home/$1/ML_Function_Schema_Data.tar.gz https://s3.amazonaws.com/verticatestdrive/ML_Function_Schema_Data.tar.gz
-wget -O /home/$1/auth.txt https://s3.amazonaws.com/verticatestdrive/auth.txt
+wget -O /home/dbadmin/clickstreamAB.tar.gz https://s3.amazonaws.com/verticatestdrive/clickstreamAB.tar.gz
+wget -O /home/dbadmin/ML_Function_Schema_Data.tar.gz https://s3.amazonaws.com/verticatestdrive/ML_Function_Schema_Data.tar.gz
+wget -O /home/dbadmin/auth.txt https://s3.amazonaws.com/verticatestdrive/auth.txt
 wget -O /tmp/java/jdk-8u121-linux-x64.rpm  https://s3.amazonaws.com/verticatestdrive/jdk-8u121-linux-x64.rpm
 wget -O /tmp/java/apache-tomcat-8.0.41.tar.gz https://s3.amazonaws.com/verticatestdrive/apache-tomcat-8.0.41.tar.gz
 wget -O /tmp/java/editprofile.txt https://s3.amazonaws.com/verticatestdrive/editprofile.txt
@@ -99,12 +112,12 @@ wget -O /tmp/java/Changedbadminpasswd.sh https://raw.githubusercontent.com/prade
 wget -O /tmp/java/ACME_ABTesting_Dashboard.zip https://s3.amazonaws.com/verticatestdrive/ACME_ABTesting_Dashboard.zip
 wget -O /tmp/java/lgx120201.lic https://s3.amazonaws.com/verticatestdrive/lgx120201.lic
 
-echo 2ndstep_DownloadEnd_GunzipStart >> /home/$1/stepfile
-gunzip /home/$1/clickstreamAB.tar.gz
+echo 2ndstep_DownloadEnd_GunzipStart >> /home/dbadmin/stepfile
+gunzip /home/dbadmin/clickstreamAB.tar.gz
 gunzip /tmp/java/apache-tomcat-8.0.41.tar.gz
-gunzip /home/$1/ML_Function_Schema_Data.tar.gz
+gunzip /home/dbadmin/ML_Function_Schema_Data.tar.gz
 
-echo 3rdstep_GzipEnd_RunRPM >> /home/$1/stepfile      	
+echo 3rdstep_GzipEnd_RunRPM >> /home/dbadmin/stepfile      	
 rpm -Uvh /tmp/java/jdk-8u121-linux-x64.rpm
 # rpm -Uvh /tmp/java/dos2unix-6.0.3-4.el7.x86_64.rpm		  
 tar -xvf /tmp/java/apache-tomcat-8.0.41.tar --directory=/opt 
@@ -119,35 +132,36 @@ cat /tmp/java/Changedbadminpasswd.sh | dos2unix  >> /tmp/java/Chg.txt
 mv /tmp/java/Chg.txt  /tmp/java/Changedbadminpasswd.sh
 source /etc/profile
 
-echo 4thstep_InstallVerticaDB >> /home/$1/stepfile 	
-sudo -n -H -u $1 /opt/vertica/bin/admintools  -t create_db -s localhost -d testdrive -c /data/controlfiles -D /data/datafiles  
-sudo -n -H -u $1 mkdir /home/$1/TestDrive 		  
-sudo -n -H -u $1 mkdir /home/$1/TestDrive/ABTesting 		  
-sudo -n -H -u $1 mkdir /home/$1/TestDrive/MLFunctions 		 
-echo 5thstep_Misc >> /home/$1/stepfile 	
-cat /home/$1/auth.txt >> /home/$1/.ssh/authorized_keys 	
-tar -xvf /home/$1/clickstreamAB.tar  --directory=/home/$1/TestDrive/ABTesting/ 	
-tar -xvf /home/$1/ML_Function_Schema_Data.tar --directory=/home/$1/TestDrive/MLFunctions/	
+echo 4thstep_InstallVerticaDB >> /home/dbadmin/stepfile 	
+sudo -n -H -u dbadmin /opt/vertica/bin/admintools  -t create_db -s localhost -d testdrive -c /data/controlfiles -D /data/datafiles  
+sudo -n -H -u dbadmin mkdir /home/dbadmin/TestDrive 		  
+sudo -n -H -u dbadmin mkdir /home/dbadmin/TestDrive/ABTesting 		  
+sudo -n -H -u dbadmin mkdir /home/dbadmin/TestDrive/MLFunctions 		 
+echo 5thstep_Misc >> /home/dbadmin/stepfile 	
+cat /home/dbadmin/auth.txt >> /home/dbadmin/.ssh/authorized_keys 	
+tar -xvf /home/dbadmin/clickstreamAB.tar  --directory=/home/dbadmin/TestDrive/ABTesting/ 	
+tar -xvf /home/dbadmin/ML_Function_Schema_Data.tar --directory=/home/dbadmin/TestDrive/MLFunctions/	
 hostname testdrive.localdomain
 echo 'testdrive.localdomain' > /etc/hostname
 sed 's/1   localhost /1   testdrive.localdomain localhost /' < /etc/hosts > /tmp/java/hosts
 cp /etc/hosts /etc/hosts.sav
 mv /tmp/java/hosts /etc/hosts
 /opt/apache-tomcat-8.0.41/bin/startup.sh
-echo End_of_Steps >> /home/$1/stepfile 
+echo End_of_Steps >> /home/dbadmin/stepfile 
 
-echo set_password >> /home/$1/stepfile
-chmod +x /tmp/java/Changedbadminpasswd.sh >>/home/$1/stepfile
-/tmp/java/Changedbadminpasswd.sh >>/home/$1/stepfile
-echo set_password_completed >> /home/$1/stepfile
+echo set_password >> /home/dbadmin/stepfile
+chmod +x /tmp/java/Changedbadminpasswd.sh >>/home/dbadmin/stepfile
+/tmp/java/Changedbadminpasswd.sh >>/home/dbadmin/stepfile
+echo set_password_completed >> /home/dbadmin/stepfile
 rm -rf /tmp/java
-rm -f /home/$1/clickstreamAB.tar
-rm -f /home/$1/ML_Function_Schema_Data.tar
-rm -f /home/$1/auth.txt
-echo Clean_up_files >> /home/$1/stepfile 
+rm -f /home/dbadmin/clickstreamAB.tar
+rm -f /home/dbadmin/ML_Function_Schema_Data.tar
+rm -f /home/dbadmin/auth.txt
+echo Clean_up_files >> /home/dbadmin/stepfile 
 chkconfig --level 12345 verticad  on
 sed -i 's/ChallengeResponseAuthentication .*no$/ChallengeResponseAuthentication yes/' /etc/ssh/sshd_config
 
+#Firewall add to the vm
 sudo firewall-cmd --zone=public --add-port=443/tcp --permanent
 sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
 sudo firewall-cmd --zone=public --add-port=8080/tcp --permanent
