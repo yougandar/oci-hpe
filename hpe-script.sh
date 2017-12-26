33ddb0acd5878c86cbb6dd79c7277fc53b3ba66a
yougandar <ynaik@sysgain.com>
2017-12-26 11:36:14 +0530

    Update hpe-script.sh

--- hpe-script.sh
+++ hpe-script.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-iqn=$1
-blockIp=$2
-iqn1=$3
-blockIp1=$4
-sudo iscsiadm -m node -o new -T $iqn -p $blockIp:3260
-sudo iscsiadm -m node -o update -T $iqn -n node.startup -v automatic
-sudo iscsiadm -m node -T $iqn -p $blockIp:3260 -l
-sudo iscsiadm -m node -o new -T $iqn1 -p $blockIp1:3260
-sudo iscsiadm -m node -o update -T $iqn1 -n node.startup -v automatic
-sudo iscsiadm -m node -T $iqn1 -p $blockIp1:3260 -l
-sudo -i
-sudo useradd dbadmin
 
 ##Install needed Packages
 yum -y install dialog
