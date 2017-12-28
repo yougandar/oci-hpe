1441d316dd321a174a0c3b47f19b5c9b341cb2c4
yougandar <ynaik@sysgain.com>
2017-12-28 15:08:42 +0530

    Update hpe-script.sh

--- hpe-script.sh
+++ hpe-script.sh
@@ -164,7 +164,10 @@ sudo firewall-cmd --zone=public --add-port=5433/tcp --permanent
 sudo firewall-cmd --reload
 sudo firewall-cmd --list-ports
 
-sleep 5
+sudo 20
+
+sudo /opt/apache-tomcat-8.0.41/bin/startup.sh
+sleep 10
 service sshd restart
 systemctl stop firewalld
 systemctl disable firewalld
